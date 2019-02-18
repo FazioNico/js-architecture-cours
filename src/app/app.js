cea947270210994ce3a2f5b4ff869aa73078b17c
FazioNico <contact@nicolasfazio.ch>
2019-02-18 09:39:14 +0100

    step 03 - display form data to console

--- src/app/app.js
+++ src/app/app.js
@@ -16,11 +16,29 @@ class MyApp {
 class LoginPage {
   constructor(appBody) {
     this.appBody = appBody;
-    this.initUI()
+    this.initUI();
+    this.loadEventsUI();
   }
 
   initUI() {
-    this.appBody.innerHTML = '<h1>Hello world Step 2</h1>'
+    this.appBody.innerHTML = `
+      <h1>Hello world Step 3</h1>
+      <form>
+        <input type="email" placeholder="your email"><br/>
+        <input type="password" placeholder="your password">
+        <button>Login</button>
+      </form>
+    `;
+  }
+
+  loadEventsUI() {
+    this.appBody.querySelector('form').addEventListener('submit', e => {
+      e.preventDefault();
+      const inputs = [...this.appBody.querySelectorAll('input')];
+      const email = inputs.find(i => i.type === 'email');
+      const password = inputs.find(i => i.type === 'password');
+      console.log('form value:', email.value, password.value);
+    })
   }
 }
 
