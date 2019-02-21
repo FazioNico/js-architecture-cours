2adf7865af4abd637931ad716804ab756983c61d
FazioNico <contact@nicolasfazio.ch>
2019-02-21 10:17:58 +0100

    step 12 add meteo component

--- src/app/features/dashboard-page/dashboard-page.js
+++ src/app/features/dashboard-page/dashboard-page.js
@@ -1,6 +1,7 @@
 import { Timer } from "../../components/timer/time-class";
 import { Say } from "../../components/say/say";
 import { Favoris } from "../../components/favoris/favoris";
+import { Meteo } from "../../components/meteo/meteo";
 
 // Class DasboardPage
 export class DasboardPage {
@@ -13,13 +14,19 @@ export class DasboardPage {
   }
   
   initUI() {
+
+
     this.appBody.innerHTML = `
     <my-timer></my-timer>
     <my-say></my-say> ${this.username}
     <my-favoris></my-favoris>
+    <my-meteo></my-meteo>
     `;
     new Timer(document.querySelector('my-timer'));
     new Say(document.querySelector('my-say'));
     new Favoris(document.querySelector('my-favoris'), this.db, this.auth);
+    new Meteo(document.querySelector('my-meteo'));
+    // .then() effet de "fade"...
+    // .catch pour choper l'erreur si il y en a une...
   }
 }
